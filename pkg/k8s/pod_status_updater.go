d57d8498ade3b2c0a4e33c27c2b5167c6b6ef471
Siddharth Agarwal <siddharth13103410@gmail.com>
2025-06-11 16:45:58 +0000

    push add-maxInflight(reqRouterIdle)

--- pkg/k8s/pod_status_updater.go
+++ pkg/k8s/pod_status_updater.go
@@ -4,40 +4,39 @@
 package k8s
 
 import (
+	"log"
 	"time"
-    "log"
 
 	providertypes "github.com/openfaas/faas-provider/types"
 )
 
 // Implement the PodStatusUpdater interface
 func (l *FunctionLookup) MarkPodBusy(podName, podIP string) error {
-    if status, exists := l.podStatusCache.Get(podName, podIP); exists {
-        l.podStatusCache.Set(podName, "busy", podIP, status.Function, status.Namespace)
-    }
-    return nil
+	if status, exists := l.podStatusCache.Get(podName, podIP); exists {
+		l.podStatusCache.Set(podName, "busy", podIP, status.Function, status.Namespace, status.MaxInflight) // SA - add maxInflight
+	}
+	return nil
 }
 
 func (l *FunctionLookup) MarkPodIdle(podName, podIP string) error {
-    if status, exists := l.podStatusCache.Get(podName, podIP); exists {
-        log.Printf("Marking pod %s as idle", podName)
-        l.podStatusCache.Set(podName, "idle", podIP, status.Function, status.Namespace)
-    }
-    return nil
+	if status, exists := l.podStatusCache.Get(podName, podIP); exists {
+		log.Printf("Marking pod %s as idle", podName)
+		l.podStatusCache.Set(podName, "idle", podIP, status.Function, status.Namespace, status.MaxInflight) // SA - add maxInflight
+	}
+	return nil
 }
 
 func (l *FunctionLookup) GetPodStatus(podName, podIP string) (providertypes.PodStatus, bool) {
-    status, exists := l.podStatusCache.Get(podName, podIP)
-    if !exists {
-        return providertypes.PodStatus{}, false
-    }
-    
-    return providertypes.PodStatus{
-        Status:    status.Status,
-        Timestamp: status.Timestamp.Format(time.RFC3339),
-        PodIP:     status.PodIP,
-        Function:  status.Function,
-        Namespace: status.Namespace,
-    }, true
-}
+	status, exists := l.podStatusCache.Get(podName, podIP)
+	if !exists {
+		return providertypes.PodStatus{}, false
+	}
 
+	return providertypes.PodStatus{
+		Status:    status.Status,
+		Timestamp: status.Timestamp.Format(time.RFC3339),
+		PodIP:     status.PodIP,
+		Function:  status.Function,
+		Namespace: status.Namespace,
+	}, true
+}
