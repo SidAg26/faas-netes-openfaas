6b6526ea6ca52f1cea34efe00db4bc860f7efffb
Siddharth Agarwal <siddharth13103410@gmail.com>
2025-06-18 05:07:28 +0000

    push add-getPodStatuses-handler-for-gateway-plugin

---
+++ pkg/handlers/pods_status_fetch.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/openfaas/faas-netes/pkg/k8s"
+)
+
+func MakePodsStatusFetchHandler(lookup *k8s.FunctionLookup) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		functionName := r.URL.Query().Get("functionName")
+		namespace := r.URL.Query().Get("namespace")
+
+		if functionName == "" || namespace == "" {
+			http.Error(w, "functionName and namespace are required", http.StatusBadRequest)
+			return
+		}
+
+		statuses, err := lookup.GetPodStatusByFunction(functionName, namespace)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if len(statuses) == 0 {
+			http.Error(w, "no pods found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(statuses); err != nil {
+			http.Error(w, "failed to encode response", http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
